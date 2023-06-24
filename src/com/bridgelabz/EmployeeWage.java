bb474b2d544054a5b0a06b70a8985f935ea5ee6b
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-06-24 18:42:03 +0530

    Employees is present or absent

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,13 +2,14 @@ package com.bridgelabz;
 
 public class EmployeeWage {
     public static void main(String[] args) {
+
         double empCheck = Math.floor(Math.random()*10)%2;
-        System.out.println("Welcome to employee wage");
-        
+
         if (empCheck == 1){
-            System.out.println("Employee is present");
+            System.out.println("Present");
         }else {
-            System.out.println("Employee is absent");
+            System.out.println("Absent");
         }
+       
     }
 }
