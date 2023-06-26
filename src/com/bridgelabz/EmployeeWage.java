8ac6897108833026d7309b87387b1e3256fcc09a
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-06-26 23:47:21 +0530

    Calculate Wages till a condition of total working hours or days is reached for a month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,17 +1,21 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    static final int EMPLOYEE_RATE_PER_HOUR = 20;
+    static final int IS_PART_TIME = 1;
+    static final int IS_FULL_TIME = 2;
+    static final int TOTAL_WORKING_DAYS = 20;
+    static final int TOTAL_WORKING_HOURS = 100;
     public static void main(String[] args) {
-        int EMPLOYEE_RATE_PER_HOUR = 20;
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int TOTAL_WORKING_DAYS = 20;
 
         int employeeTotalWage = 0;
         int employeeHours = 0;
         int employeeWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        for (int i = 1; i <= TOTAL_WORKING_DAYS; i++) {
+        int day = 1;
+        int workingHour = 0;
+
+        while (day <= TOTAL_WORKING_DAYS && workingHour <= TOTAL_WORKING_HOURS){
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case 0:
                     employeeHours = 0;
@@ -25,10 +29,15 @@ public class EmployeeWage {
                 default:
                     System.out.println("Invalid choice");
             }
-            employeeWage = employeeHours * EMPLOYEE_RATE_PER_HOUR;
-            System.out.println(employeeWage);
-            employeeTotalWage += employeeWage;
+        employeeWage = employeeHours * EMPLOYEE_RATE_PER_HOUR;
+        employeeTotalWage += employeeWage;
+        workingHour+=employeeHours;
+        System.out.println("Day: " + day);
+        System.out.println("Daily employee wage: " + employeeWage);
+        System.out.println("Working hour: " + workingHour);
+        System.out.println();
+        day++;
         }
-        System.out.println(employeeTotalWage);
+        System.out.println("Total employee wage after 20 days or 100 hours: " + employeeTotalWage);
     }
 }
