ff3c6cbe44b3bee13e4930728e5e522e61263ef5
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-06-27 09:48:05 +0530

    Compute Employee Wage for multiple companies

--- src/com/bridgelabz/Method.java
+++ src/com/bridgelabz/Method.java
@@ -6,7 +6,7 @@ public class Method {
         company1.employeeWage(5,10,20,150,25);
         System.out.println("For company 1: ");
         System.out.println();
-        
+
         System.out.println("*********************************");
         System.out.println();
         Company company2 = new Company();
