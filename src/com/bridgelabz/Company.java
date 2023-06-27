2a400cf7309f48677edab96ebfedcc85a7f2ce34
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-06-27 09:26:16 +0530

    Compute Employee Wage for multiple companies

--- src/com/bridgelabz/Company.java
+++ src/com/bridgelabz/Company.java
@@ -1,30 +1,31 @@
 package com.bridgelabz;
 
-public class Variable {
-    static final int EMPLOYEE_RATE_PER_HOUR = 20;
+public class Company {
+  //  static final int EMPLOYEE_RATE_PER_HOUR = 20;
     static final int IS_PART_TIME = 1;
     static final int IS_FULL_TIME = 2;
-    static final int TOTAL_WORKING_DAYS = 20;
-    static final int TOTAL_WORKING_HOURS = 100;
-    public void employeeWage() {
+  //  static final int TOTAL_WORKING_DAYS = 20;
+  //  static final int TOTAL_WORKING_HOURS = 100;
+    public void employeeWage(int PART_DAY_HOUR,int FULL_DAY_HOUR,int TOTAL_WORKING_DAYS,int TOTAL_WORKING_HOURS,int EMPLOYEE_RATE_PER_HOUR) {
 
         int employeeTotalWage = 0;
         int employeeHours = 0;
-        int employeeWage = 0;
+
         int day = 1;
         int workingHour = 0;
 
         while (day <= TOTAL_WORKING_DAYS && workingHour <= TOTAL_WORKING_HOURS){
+            int employeeWage = 0;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case 0:
                     employeeHours = 0;
                     break;
                 case 1:
-                    employeeHours = 4;
+                    employeeHours = PART_DAY_HOUR;
                     break;
                 case 2:
-                    employeeHours = 8;
+                    employeeHours = FULL_DAY_HOUR;
                     break;
                 default:
                     System.out.println("Invalid choice");
@@ -37,11 +38,12 @@ public class Variable {
             System.out.println("Working hour: " + workingHour);
             System.out.println();
             day++;
-            if(workingHour>100){
-                workingHour=100;
+            if(workingHour>TOTAL_WORKING_HOURS){
+                workingHour=TOTAL_WORKING_HOURS;
                 employeeTotalWage=workingHour*EMPLOYEE_RATE_PER_HOUR;
             }
         }
-        System.out.println("Total employee wage after 20 days or 100 hours: " + employeeTotalWage);
+
+        System.out.println("Total employee wage after " +TOTAL_WORKING_DAYS+ " days or " + TOTAL_WORKING_HOURS +" hours: " + employeeTotalWage);
     }
 }
--- src/com/bridgelabz/Method.java
+++ src/com/bridgelabz/Method.java
@@ -2,7 +2,15 @@ package com.bridgelabz;
 
 public class Method {
     public static void main(String[] args) {
-        Variable obj =new Variable();
-        obj.employeeWage();
+        Company company1 =new Company();
+        company1.employeeWage(5,10,20,150,25);
+        System.out.println("For company 1: ");
+        System.out.println();
+        
+        System.out.println("*********************************");
+        System.out.println();
+        Company company2 = new Company();
+        company2.employeeWage(6,12,25,200,30);
+        System.out.println("For company 2: ");
     }
 }
